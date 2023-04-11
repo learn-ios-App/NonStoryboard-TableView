4664aed7328850068a064621b30ad5bfa76c7ddb
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-04-11 11:08:29 +0900

    Initial Commit

---
+++ NonStoryboard-TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NonStoryboard-TableView
+//
+//  Created by 渡邊魁優 on 2023/04/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
